afa983348c82c1073cccfefa8db4a2116c179dce
danielBuGo <danielbuilesgomez@gmail.com>
2024-03-25 13:15:30 -0500

    Se cambia codigo en el nombre de la foto

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
         imagen = 'diedo'
         cargo = 'Software Engineer'
         empresa = 'Amazon'
-        testimonio = "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."/>
+        testimonio = "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life." 
+        persona = 'foto de Shawn' />
       <Testimonio 
         nombre = 'Sarah Chima'
         pais = 'Nigeria'
@@ -20,7 +21,8 @@ function App() {
         cargo = 'Software Engineer'
         empresa = 'ChatDesk'
         testimonio = "freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
-        />
+        persona = 'Foto de Sarah' />
+        
       <Testimonio 
         nombre = 'Emma Bostian'
         pais = 'Sweden'
@@ -28,7 +30,7 @@ function App() {
         cargo = 'Software Engineer'
         empresa = 'Spotify'
         testimonio = "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
-        />  
+        persona = 'foto de Emma' />         
       </div>
     </div>
   );
--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -6,7 +6,7 @@ function Testimonio(props) {
     <div className="contenedor-testimonio" >
       <img className="imagen-testimonio" 
       src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-      alt="Foto de diedo"/>
+      alt={props.persona} />
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
         <p className="cargo-testimonio">{props.cargo} at <strong>{props.empresa}</strong></p>
